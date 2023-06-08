d44df2950a22633740dbde9f78153ea8a55d5895
muk7777 <muk8620@gmail.com>
2023-06-08 16:50:33 +0900

    ãfirst

--- day09/_05_Exception1/Exception1.java
+++ day09/_05_Exception1/Exception1.java
@@ -1,17 +1,28 @@
 package day09._05_Exception1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exception1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        while (true){
-            System.out.println("숫자를 하나 입력해주세요 : ");
-            int val = sc.nextInt();
-            System.out.printf("당신이 입력한 값은 : %d\n", val);
+        while(true) {
+            System.out.println("숫자 하나를 입력해주세요 : ");
+
+            try { // 예외가 발생할 만한 코드
+                int val = scan.nextInt();
+                scan.nextLine();
+                System.out.printf("당신이 입력한 값은 : %d\n", val);
+
+            } catch (InputMismatchException e) { // 예외가 발생했을 때 처리
+                System.out.println("숫자만 넣어주세요.");
+                scan.nextLine();
+            }
+
+
+
         }
         // a를 입력한 경우 -> 숫자만 넣어주세요.
-
     }
 }
