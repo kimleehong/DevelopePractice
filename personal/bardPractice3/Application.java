8a6a678da027f223415b0f67d5c53c3fe0966886
muk7777 <muk8620@gmail.com>
2023-06-15 15:49:05 +0900

    first

--- personal/bardPractice3/Application.java
+++ personal/bardPractice3/Application.java
@@ -19,104 +19,17 @@ public class Application {
                 systemcontroller.exit();
                 return;
             } else if (cmd.equals("add")) {
-                System.out.print("게시물 제목을 입력해주세요 : ");
-                String title = sc.nextLine();
-                System.out.print("게시물 내용을 입력해주세요 : ");
-                String content = sc.nextLine();
-
-                titles.add(title);
-                contents.add(content);
-                registrationDate.add(formattedNow());
-                ids.add(id);
-                id++;
-
-                System.out.println("게시물이 등록되었습니다.");
+                mainController.add();
             } else if (cmd.equals("list")) {
-                if (titles.isEmpty()) {
-                    System.out.println("게시물이 존재하지 않습니다.");
-                    continue;
-                } else {
-                    System.out.println("==================");
-                    for (int i = 0; i < titles.size(); i++) {
-                        System.out.println("번호 : " + ids.get(i));
-                        System.out.println("제목 : " + titles.get(i));
-                        System.out.println("==================");
-                    }
-                }
+                mainController.list();
             } else if (cmd.equals("update")) {
-                System.out.println("수정할 게시물 번호 : ");
-                int index = Integer.parseInt(sc.nextLine());
-
-                int target = findTargetIndex(index);
-
-                if (target == -1) {
-                    System.out.println("없는 게시물 번호입니다.");
-                    continue;
-                } else {
-                    System.out.print("제목 : ");
-                    String newTitle = sc.nextLine();
-                    System.out.print("내용 : ");
-                    String newContent = sc.nextLine();
-
-                    titles.set(target, newTitle);
-                    contents.set(target, newContent);
-
-                    System.out.println(index + "번 게시물이 수정되었습니다.");
-                }
+                mainController.update();
             } else if (cmd.equals("delete")) {
-                System.out.println("삭제할 게시물 번호 : ");
-                int index = Integer.parseInt(sc.nextLine());
-
-                int target = findTargetIndex(index);
-
-                if (target == -1) {
-                    System.out.println("없는 게시물 번호입니다.");
-                    continue;
-                } else {
-                    titles.remove(target);
-                    contents.remove(target);
-                    ids.remove(target);
-                    registrationDate.remove(target);
-                    views.remove(target);
-                    System.out.println(index + "번 게시물이 삭제되었습니다.");
-                }
+                mainController.delete();
             } else if (cmd.equals("detail")) {
-                System.out.print("상세보기 할 게시물 번호를 입력해주세요 : ");
-                int index = Integer.parseInt(sc.nextLine());
-
-                int target = findTargetIndex(index);
-
-                if (target == -1) {
-                    System.out.println("없는 게시물 번호입니다.");
-                    continue;
-                } else {
-                    System.out.println("==================");
-                    System.out.println("번호 : " + ids.get(target));
-                    System.out.println("제목 : " + titles.get(target));
-                    System.out.println("내용 : " + contents.get(target));
-                    System.out.println("등록날짜 : " + registrationDate.get(target));
-                    int newViews = views.get(target) + 1;
-                    views.set(target, newViews);
-                    System.out.println("조회수 : " + views.get(target));
-                    System.out.println("==================");
-                }
+                mainController.detail();
             } else if (cmd.equals("search")) {
-                System.out.print("검색 키워드를 입력해 주세요: ");
-                String keyword = sc.nextLine();
-                boolean found = false;
-                System.out.println("=============");
-                for (int i = 0; i < ids.size(); i++) {
-                    String a = titles.get(i);
-                    if (a.contains(keyword)) {
-                        System.out.println("=============");
-                        System.out.println("번호: " + ids.get(i));
-                        System.out.println("제목: " + titles.get(i));
-                        found = true;
-                    }
-                }
-                if (!found) {
-                    System.out.println("검색 결과가 없습니다.");
-                }
+                mainController.search();
             } else {
                 System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
             }
--- personal/bardPractice3/article/controller/MainController.java
+++ personal/bardPractice3/article/controller/MainController.java
@@ -12,8 +12,9 @@ public class MainController {
     Scanner sc = new Scanner(System.in);
     List<Entity> entities = new ArrayList<>();
     int id = 1;
+
     public void initData() {
-        Entity entity1 = new Entity(id,"안녕하세요 반갑습니다. 자바 공부중이에요.","첫번째 내용");
+        Entity entity1 = new Entity(id, "안녕하세요 반갑습니다. 자바 공부중이에요.", "첫번째 내용");
         entity1.setRegistrationDates(formattedNow());
         entities.add(entity1);
         id++;
@@ -23,7 +24,7 @@ public class MainController {
         entities.add(entity2);
         id++;
 
-        Entity entity3 = new Entity(id,"정처기 따야되나요?","세번째 내용");
+        Entity entity3 = new Entity(id, "정처기 따야되나요?", "세번째 내용");
         entity3.setRegistrationDates(formattedNow());
         entities.add(entity3);
         id++;
@@ -44,4 +45,109 @@ public class MainController {
         }
         return null;
     }
+
+    public void add() {
+        System.out.print("게시물 제목을 입력해주세요 : ");
+        String title = sc.nextLine();
+        System.out.print("게시물 내용을 입력해주세요 : ");
+        String content = sc.nextLine();
+
+        Entity entity = new Entity(id,title,content);
+        entity.setRegistrationDates(formattedNow());
+        entities.add(entity);
+        id++;
+
+        System.out.println("게시물이 등록되었습니다.");
+    }
+
+    public void list() {
+        if (entities.isEmpty()) {
+            System.out.println("게시물이 존재하지 않습니다.");
+        } else {
+            System.out.println("==================");
+            for (Entity entity : entities) {
+                System.out.println("번호 : " + entity.getIds());
+                System.out.println("제목 : " + entity.getTitles());
+                System.out.println("==================");
+            }
+        }
+    }
+
+    public void update() {
+        System.out.println("수정할 게시물 번호 : ");
+        int index = Integer.parseInt(sc.nextLine());
+
+        Entity entity = findEntityById(index);
+
+        if (entity == null) {
+            System.out.println("없는 게시물 번호입니다.");
+        } else {
+            System.out.print("제목 : ");
+            String newTitle = sc.nextLine();
+            System.out.print("내용 : ");
+            String newContent = sc.nextLine();
+
+            entity.setTitles(newTitle);
+            entity.setContents(newContent);
+            entity.setRegistrationDates(formattedNow());
+
+            System.out.println(index + "번 게시물이 수정되었습니다.");
+        }
+    }
+
+    public void delete() {
+        System.out.println("삭제할 게시물 번호 : ");
+        int index = Integer.parseInt(sc.nextLine());
+
+        Entity entity = findEntityById(index);
+
+        if (entity == null) {
+            System.out.println("없는 게시물 번호입니다.");
+        } else {
+            entities.remove(entity);
+            System.out.println(index + "번 게시물이 삭제되었습니다.");
+        }
+    }
+
+    public void detail() {
+        System.out.print("상세보기 할 게시물 번호를 입력해주세요 : ");
+        int index = Integer.parseInt(sc.nextLine());
+
+        Entity entity = findEntityById(index);
+
+        if (entity == null) {
+            System.out.println("없는 게시물 번호입니다.");
+        } else {
+            System.out.println("==================");
+            System.out.println("번호 : " + entity.getIds());
+            System.out.println("제목 : " + entity.getTitles());
+            System.out.println("내용 : " + entity.getContents());
+            System.out.println("등록날짜 : " + entity.getRegistrationDates());
+
+            entity.setViews(entity.getViews() + 1);
+
+            System.out.println("조회수 : " + entity.getViews());
+            System.out.println("==================");
+        }
+    }
+
+    public void search() {
+        System.out.print("검색 키워드를 입력해 주세요: ");
+        String keyword = sc.nextLine();
+        boolean found = false;
+        System.out.println("=============");
+        for (int i = 0; i < entities.size(); i++) {
+            Entity a = entities.get(i);
+            if (a.getTitles().contains(keyword)) {
+                System.out.println("=============");
+                System.out.println("번호: " + a.getIds());
+                System.out.println("제목: " + a.getTitles());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("검색 결과가 없습니다.");
+        }
+    }
 }
+
